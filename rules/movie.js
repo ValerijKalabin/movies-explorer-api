2afc2f3dede0c9e0bc0fabfda30301754ed43a0f
Valerij Kalabin <valerijkalabin@yandex.ru>
2021-02-12 21:14:03 +0700

    create a rules

---
+++ rules/movie.js
@@ -0,0 +1,22 @@
+const { Joi } = require('celebrate');
+
+module.exports.movieRule = {
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.string().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    image: Joi.string().required().uri(),
+    thumbnail: Joi.string().required().uri(),
+    trailer: Joi.string().required().uri(),
+  }).unknown(true),
+};
+
+module.exports.movieIdRule = {
+  params: Joi.object().keys({
+    cardId: Joi.string().required().length(24),
+  }),
+};
